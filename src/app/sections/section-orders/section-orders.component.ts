d860bf04ff8278a3cec639efbfcfeb933adcaec5
nrholm1 <nielsraunkjaer@gmail.com>
2020-05-13 10:33:38 +0200

    Set up template for orders table, populated with mock data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {id: 1, customer: {id: 1, name: "Acme Hosting", state: "CA", email: "contact@acmehosting.com"}, total: 230, placed: new Date(2020, 11, 9), fulfilled: new Date(), status: "Completed"},
+    {id: 1, customer: {id: 2, name: "Acme Hosting", state: "CA", email: "contact@acmehosting.com"}, total: 230, placed: new Date(2020, 10, 14), fulfilled: new Date(), status: "Completed"},
+    {id: 1, customer: {id: 3, name: "Acme Hosting", state: "CA", email: "contact@acmehosting.com"}, total: 230, placed: new Date(2020, 11, 17), fulfilled: new Date(), status: "Completed"},
+    {id: 1, customer: {id: 4, name: "Acme Hosting", state: "CA", email: "contact@acmehosting.com"}, total: 230, placed: new Date(2020, 8, 4), fulfilled: new Date(), status: "Completed"},
+    {id: 1, customer: {id: 5, name: "Acme Hosting", state: "CA", email: "contact@acmehosting.com"}, total: 230, placed: new Date(2020, 9, 21), fulfilled: new Date(), status: "Completed"},
+  ];
 
   ngOnInit(): void {
   }
